35ef74541bb3f70052e840f90ae8d7d72de35789
jessehancock <jessehancock1@gmail.com>
2016-10-03 17:02:13 -0500

    yah

---
+++ 7_keepItSimpleStupid.js
@@ -0,0 +1,8 @@
+function isKiss( words ){
+  var arr = words.split(' ');
+  var length = arr.length;
+  for(var i = 0; i < length; i++){
+    if(arr[i].length > length) return "Keep It Simple Stupid"
+  }
+  return "Good work Joe!"
+}
