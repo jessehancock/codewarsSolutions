b89df9651f52f74f951552a445db35d72cf49938
jessehancock <jessehancock1@gmail.com>
2016-10-08 09:22:01 -0500

    fast code

---
+++ 7_coding3Mins.js
@@ -0,0 +1,17 @@
+function findAB(numbers,c){
+  if(c === 0){
+  for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] === 0) return [numbers[0], 0]
+    }
+    return null
+  }
+  var values = numbers.filter((element) => c % element ===0);
+  console.log(values);
+  if(values.length < 2) return null;
+  for(var i = 0; i < values.length -1; i++){
+   for(var j = i+1; j < values.length; j++){
+    if(values[i] * values[j] === c) return [values[i], values[j]];
+    }
+  }
+  return null
+}
